e8f884fd902b01e4784a6328db661c7f35ef4dc0
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-08-07 17:00:27 -0600

    slight changes

--- gpt.py
+++ gpt.py
@@ -1,19 +1,17 @@
 import openai
 import os
 
-# Load environment variables from .env file
+# to prevent api key from getting leaked
 from dotenv import load_dotenv
 load_dotenv()
-
-# Read the API key from the environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def chat_with_gpt(messages):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
-        max_tokens=50,  # Adjust as needed
-        temperature=0.7  # Adjust as needed
+        max_tokens=50, 
+        temperature=0.7  
     )
     return response.choices[0].message["content"]
 
