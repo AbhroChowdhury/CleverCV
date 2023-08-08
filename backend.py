0f228e24523cc13947b9bbaf35defa54cfc523e9
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-08-07 20:42:50 -0600

    new way to extract pdf

--- backend.py
+++ backend.py
@@ -11,13 +11,12 @@ load_dotenv()
 # Read the API key from the environment variable
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        text = ''
-        for page_num in range(len(pdf_reader.pages)):
-            page = pdf_reader.pages[page_num]
-            text += page.extract_text()
+def extract_text_from_pdf(pdf_file):
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    text = ''
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
     return text
 
 def identify_bullet_points(text):
